caceb00975605258392902dd61c71038429eca9a
admin <admin>
2023-04-27 01:14:36 +0300

    add Makefile

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,5 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    Scanner sc = new Scanner(System.in);
+    public static void welcomeUser(){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next ();
+        System.out.println ("May I have your name? " + name);
+        System.out.println ("Hello, " + name + "!");
+    }
+
 }
