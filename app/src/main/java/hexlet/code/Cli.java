faac0f68e9829397fe930bfd8287a1c26dfaf735
admin <admin>
2023-04-27 10:20:10 +0300

    add Scanner.in

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Cli {
     public static void welcomeUser(){
         Scanner sc = new Scanner(System.in);
+        System.out.print ("May I have your name? ");
         String name = sc.next ();
-        System.out.println ("May I have your name? " + name);
         System.out.println ("Hello, " + name + "!");
     }
 
