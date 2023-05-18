6060c607ea61e8df6fead8474e2be3ef3279c457
admin <admin>
2023-05-18 14:03:16 +0300

    add new Games

--- app/src/main/java/hexlet/code/games/GamePrime.java
+++ app/src/main/java/hexlet/code/games/GamePrime.java
@@ -9,7 +9,6 @@ public class GamePrime {
     public static String description;
     public static String[][] questionsAndAnswers;
     public static boolean prime;
-    public static int number;
 
     public static void startGame() {
         Scanner sc = new Scanner(System.in);
@@ -25,11 +24,11 @@ public class GamePrime {
     }
 
     private static boolean isPrime(int number) {
-        for (int i = 2; i <= number / 2; i++) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
-                prime = false;
+                return prime = false;
             } else {
-                prime = true;
+                return prime = true;
             }
         }
         return prime;
