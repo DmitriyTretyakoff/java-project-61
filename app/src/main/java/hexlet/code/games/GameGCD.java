354a5e7f78866b17649b592851f595ffa9dd05d9
admin <admin>
2023-05-19 13:12:30 +0300

    add new Games

--- app/src/main/java/hexlet/code/games/GameGCD.java
+++ app/src/main/java/hexlet/code/games/GameGCD.java
@@ -11,7 +11,7 @@ public class GameGCD {
         for (int i = 0; i < Engine.COUNT_ROUNDS; i++) {
             int number1 = Utils.generateNumber(100) + 1;
             int number2 = Utils.generateNumber(100) + 1;
-            questionsAndAnswers[i][0] = number1 + " " + number2;
+            questionsAndAnswers[i][0] = String.valueOf(number1 + " " + number2);
             questionsAndAnswers[i][1] = String.valueOf(divisor(number1, number2));
         }
         Engine.runGame(description, questionsAndAnswers);
