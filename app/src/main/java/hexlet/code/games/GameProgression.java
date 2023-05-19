7829a4c8a5e9ee3554869f4228be62dcb4c839e9
admin <admin>
2023-05-19 12:05:20 +0300

    add new Games

--- app/src/main/java/hexlet/code/games/GameProgression.java
+++ app/src/main/java/hexlet/code/games/GameProgression.java
@@ -2,43 +2,43 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GameProgression {
-    public static String description;
-    public static String[][] questionsAndAnswers;
-    public static int[] progression;
-    public static int length;
+    public static String description = "What number is missing in the progression?";
+    public static String[][] questionsAndAnswers = new String[3][2];
     public static int delta;
     public static int hiddenIndex;
     public static int firstNumber;
 
     public static void startGame() {
-        Scanner sc = new Scanner (System.in);
-        Random random = new Random ();
-        description = "What number is missing in the progression?";
-        questionsAndAnswers = new String[3][2];
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
         for (int i = 0; i < Engine.COUNT_ROUNDS; i++) {
-            int length = random.nextInt (6) + 5;
-            int[] progression = new int[length];
-            hiddenIndex = random.nextInt (length);
-            firstNumber = random.nextInt (11);
-            delta = random.nextInt (3) + 1;
-            questionsAndAnswers[i][0] = String.valueOf (progression[i] + " ");
-            questionsAndAnswers[i][1] = findNumber ();
+            String[] progression = generateProgression();
+            hiddenIndex = random.nextInt(progression.length);
+            int hidden = Integer.parseInt(progression[hiddenIndex]);
+            questionsAndAnswers[i][0] = Arrays.toString(buildQuestion(progression));
+            questionsAndAnswers[i][1] = String.valueOf(hidden);
         }
+        Engine.runGame(description, questionsAndAnswers);
     }
 
-    private static String findNumber() {
+    private static String[] generateProgression() {
+        int length = Utils.generateNumber(6) + 5;
+        String[] progression = new String[length];
+        firstNumber = Utils.generateNumber(11);
+        delta = Utils.generateNumber(3) + 1;
         for (int i = 0; i < length; i++) {
-            progression[i] = firstNumber + i * delta;
-            if (i == hiddenIndex) {
-                System.out.print (".. ");
-            } else {
-                System.out.print (progression[i] + " ");
-            }
+            progression[i] = String.valueOf(firstNumber + i * delta);
         }
-        return String.valueOf (hiddenIndex);
+        return progression;
+    }
+
+    private static String[] buildQuestion(String[] progression) {
+        progression[hiddenIndex] = "..";
+        return progression;
     }
 }
