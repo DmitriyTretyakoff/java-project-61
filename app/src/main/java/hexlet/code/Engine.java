9d2a07ed64790a1dcd95575d78585441503f9ac4
admin <admin>
2023-06-01 11:04:49 +0300

    add new Progression

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Engine {
     public static final int COUNT_ROUNDS = 3;
 
-    public static void runGame(String DESCRIPTION, String[][] QUESTIONS_ANSWERS) {
+    public static void runGame(String description, String[][] questionsAnwered) {
         Scanner sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = sc.next();
         System.out.println("Hello, " + userName + "!");
         String answer;
-        System.out.println(DESCRIPTION);
+        System.out.println(description);
         for (int i = 0; i < COUNT_ROUNDS; i++) {
-            System.out.println("Question: " + QUESTIONS_ANSWERS[i][0]);
+            System.out.println("Question: " + questionsAnwered[i][0]);
             answer = sc.next();
-            if (QUESTIONS_ANSWERS[i][1].equals(answer)) {
+            if (questionsAnwered[i][1].equals(answer)) {
                 System.out.println("Correct!");
             } else {
-                System.out.println("'" + answer + "' is wrong answer ;" + "(. Correct answer was '" + QUESTIONS_ANSWERS[i][1] + "'.");
+                System.out.println("'" + answer + "' is wrong answer ;" + "(. Correct answer was '" + questionsAnwered[i][1] + "'.");
                 System.out.println("Let's try again, " + userName + "!");
                 return;
             }
